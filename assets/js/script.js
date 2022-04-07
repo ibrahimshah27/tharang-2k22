306cc91efc807798c588e9da54ad40bd639a4fef
deosaju <deosaju2260@gmail.com>
2022-04-07 10:55:27 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,32 +161,6 @@
 		}
 
 		/**
-		 * Google map function for getting latitude and longitude
-		 */
-		function getLatLngObject(str, marker, map, callback) {
-			var coordinates = {};
-			try {
-				coordinates = JSON.parse(str);
-				callback(new google.maps.LatLng(
-					coordinates.lat,
-					coordinates.lng
-				), marker, map)
-			} catch (e) {
-				map.geocoder.geocode({'address': str}, function (results, status) {
-					if (status === google.maps.GeocoderStatus.OK) {
-						var latitude = results[0].geometry.location.lat();
-						var longitude = results[0].geometry.location.lng();
-
-						callback(new google.maps.LatLng(
-							parseFloat(latitude),
-							parseFloat(longitude)
-						), marker, map)
-					}
-				})
-			}
-		}
-
-		/**
 		 * initJplayerBase
 		 * @description  base Jplayer init obj
 		 */
@@ -763,85 +737,6 @@
 			}, 2000);
 		}
 
-		// RD Google Maps
-		if (plugins.rdGoogleMaps.length) {
-			$.getScript("//maps.google.com/maps/api/js?key=AIzaSyAwH60q5rWrS8bXwpkZwZwhw9Bw0pqKTZM&sensor=false&libraries=geometry,places&v=3.7", function () {
-				var head = document.getElementsByTagName('head')[0],
-					insertBefore = head.insertBefore;
-
-				head.insertBefore = function (newElement, referenceElement) {
-					if (newElement.href && newElement.href.indexOf('//fonts.googleapis.com/css?family=Roboto') !== -1 || newElement.innerHTML.indexOf('gm-style') !== -1) {
-						return;
-					}
-					insertBefore.call(head, newElement, referenceElement);
-				};
-
-				for (var i = 0; i < plugins.rdGoogleMaps.length; i++) {
-					var $googleMapItem = $(plugins.rdGoogleMaps[i]);
-
-					lazyInit($googleMapItem, $.proxy(function () {
-						var $this = $(this),
-							styles = $this.attr("data-styles");
-
-						$this.googleMap({
-							marker: {
-								basic: $this.data('marker'),
-								active: $this.data('marker-active')
-							},
-							styles: styles ? JSON.parse(styles) : [],
-							onInit: function (map) {
-								var inputAddress = $('#rd-google-map-address');
-
-
-								if (inputAddress.length) {
-									var input = inputAddress;
-									var geocoder = new google.maps.Geocoder();
-									var marker = new google.maps.Marker(
-										{
-											map: map,
-											icon: $this.data('marker-url')
-										}
-									);
-
-									var autocomplete = new google.maps.places.Autocomplete(inputAddress[0]);
-									autocomplete.bindTo('bounds', map);
-									inputAddress.attr('placeholder', '');
-									inputAddress.on('change', function () {
-										$("#rd-google-map-address-submit").trigger('click');
-									});
-									inputAddress.on('keydown', function (e) {
-										if (e.keyCode === 13) {
-											$("#rd-google-map-address-submit").trigger('click');
-										}
-									});
-
-
-									$("#rd-google-map-address-submit").on('click', function (e) {
-										e.preventDefault();
-										var address = input.val();
-										geocoder.geocode({'address': address}, function (results, status) {
-											if (status === google.maps.GeocoderStatus.OK) {
-												var latitude = results[0].geometry.location.lat();
-												var longitude = results[0].geometry.location.lng();
-
-												map.setCenter(new google.maps.LatLng(
-													parseFloat(latitude),
-													parseFloat(longitude)
-												));
-												marker.setPosition(new google.maps.LatLng(
-													parseFloat(latitude),
-													parseFloat(longitude)
-												))
-											}
-										});
-									});
-								}
-							}
-						});
-					}, $googleMapItem));
-				}
-			});
-		}
 
 		// Add custom styling options for input[type="radio"]
 		if (plugins.radio.length) {
